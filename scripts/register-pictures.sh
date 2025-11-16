6976f02a165b38d4418e14bf57475c616a5ed26c
Coko <91132775+Coko7@users.noreply.github.com>
2025-11-16 13:10:08 +0100

    scripts: add script to register pictures

---
+++ scripts/register-pictures.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+json_file="data/picures-generated-$(date +%Y%m%d_%H%M%S).json"
+echo '[]' > "$json_file"
+
+pictures=$(fd . -e jpg data/raw/)
+id=0
+
+for picture in $pictures; do
+    filename=$(basename "$picture")
+    computed_path="day-pics/$filename"
+
+    datetime=$(exiftool -DateTimeOriginal -s -s -s "$picture")
+
+    date=${datetime%% *}   # Extract part before first space (date)
+    date=$(echo "$date" | tr ':' '/')
+    time=${datetime##* }   # Extract part after last space (time)
+    time=$(echo "$time" | cut -d ':' -f1,2)
+
+    entry=$(jq --null-input \
+        --argjson id "$id" \
+        --arg path "$computed_path" \
+        --arg date "$date" \
+        --arg time "$time" \
+        '{
+            id: $id, path: $path,
+            original_date: $date, time_taken: $time,
+            location: ""
+        }')
+
+    echo "Created JSON: $entry"
+    jq --argjson entry "$entry" '. += [$entry]' "$json_file" > tmp.$$.json \
+        && mv tmp.$$.json "$json_file"
+
+    exiftool -all= "$picture"
+
+    ((id++))
+done
