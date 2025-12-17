55c7d6d8e8445d4575132f39623a0fbf041787e5
Coko <91132775+Coko7@users.noreply.github.com>
2025-12-17 18:00:04 +0100

    style(leaderboard): fix rank numbers with JS for now

--- src/scripts/leaderboard.js
+++ src/scripts/leaderboard.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const chkShowHiddenPlayers = document.querySelector(
-    "#chk-show-hidden-players",
-  );
-  const hiddenPlayersTr = document.querySelectorAll("tr.hidden");
+  const checkboxShowHidden = document.querySelector("#chk-show-hidden-players");
+  const playerRows = document.querySelectorAll("tr");
 
   function toggleHiddenPlayers() {
-    const isChecked = chkShowHiddenPlayers.checked;
-    hiddenPlayersTr.forEach((element) => {
-      element.style.display = isChecked ? "" : "none";
+    const showHidden = checkboxShowHidden.checked;
+    let index = 1;
+    playerRows.forEach((playerRow) => {
+      const isHiddenPlayer = playerRow.classList.contains("hidden");
+      if (!isHiddenPlayer || showHidden) {
+        playerRow.style.display = "";
+        const rankCell = playerRow.querySelector("td.rank");
+        if (rankCell) rankCell.textContent = index++;
+      } else {
+        playerRow.style.display = "none";
+      }
     });
   }
 
-  chkShowHiddenPlayers.addEventListener("change", toggleHiddenPlayers);
+  checkboxShowHidden.addEventListener("change", toggleHiddenPlayers);
   toggleHiddenPlayers(); // Initial state
 });
